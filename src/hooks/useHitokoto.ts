fb5a7a7f8091cdb480b45b2b3b287a57ff326ab1
wanglixiang <xsmarter@outlook.com>
2025-06-21 17:22:20 +0800

    chore: 删除无用代码

--- src/hooks/useHitokoto.ts
+++ src/hooks/useHitokoto.ts
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import { HITOKOTO_API } from "@/config/constants";
 
-import { getNotionData } from "@/lib/notion/getNotionData";
-
 interface HitokotoData {
   hitokoto: string;
   from: string;
@@ -16,10 +14,6 @@ export const useHitokoto = () => {
 
   const fetchHitokoto = async () => {
     try {
-      const notionresponse = await getNotionData();
-
-      console.log("从 Notion 获取到的数据:", notionresponse);
-
       const response = await fetch(HITOKOTO_API);
       const jsonData = await response.json();
       setData(jsonData);
